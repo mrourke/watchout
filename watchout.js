53ceb43930a26ec4d611e9823d225a7bd6ca9670
Matthew Rourke <mrourke@gmail.com>
2015-04-03 19:50:16 -0400

    Player movement on drag implemented

--- watchout.js
+++ watchout.js
@@ -16,17 +16,40 @@ for (var i = 0; i < 30; i++) {
   });
 }
 
+var player = {
+  'x': width / 2,
+  'y': height / 2
+}
+
+var drag = d3.behavior.drag()
+  //.origin(function(d) { return d; })
+  .on('drag', function(d) {
+    //svg.select('circle.player')
+    d3.select(this)
+      .attr('cx', d3.event.x)
+      .attr('cy', d3.event.y);
+  });
+
 svg.selectAll('circle').data(enemies, function(index) {
   return index.i;
 }).enter().append('circle')
+  .attr('class', 'enemy')
   .attr('r', 10)
-  .attr('color', 'black')
+  //.attr('color', 'black')
   .attr('cx', function(data, index) {
     return enemies[index].x;
   })
   .attr('cy', function(data, index){
     return enemies[index].y;
   });
+
+svg.append('circle')
+  .attr('class', 'player')
+  .attr('fill', '#ff0000')
+  .attr('r', 10)
+  .attr('cx', player.x)
+  .attr('cy', player.y)
+  .call(drag);
 
 var enemyMove = function() {
   enemies = enemies.map(function(input) {
@@ -35,7 +58,7 @@ var enemyMove = function() {
     return input;
   });
 
-  svg.selectAll('circle').data(enemies, function(index) {
+  svg.selectAll('circle.enemy').data(enemies, function(index) {
     return index.i;
   }).transition().duration(500)
   .attr('cx', function(data, index) {
@@ -43,7 +66,9 @@ var enemyMove = function() {
   })
   .attr('cy', function(data, index){
     return enemies[index].y;
-  })
+  });
+
+  //svg.select
 };
 
 setInterval(enemyMove, 1000);
