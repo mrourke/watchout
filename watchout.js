6451c3cedd990af061f2c1d8b3073c11498ce6b6
Matthew Rourke <mrourke@gmail.com>
2015-04-03 18:20:10 -0400

    Initial SVG build

--- watchout.js
+++ watchout.js
@@ -1,2 +1,21 @@
 // start slingin' some d3 here.
 
+var width = 1000;
+var height = 500;
+
+var svg = d3.select("body").append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+var enemies = [];
+for (var i = 0; i < 30; i++) {
+  enemies.push({
+    'i': i,
+    'x': Math.floor(Math.random() * width),
+    'y': Math.floor(Math.random() * height)
+  });
+}
+
+svg.SelectAll('circle').data(enemies, function(index) {
+  return [index[x], index[y]];
+}).enter().append('circle');
